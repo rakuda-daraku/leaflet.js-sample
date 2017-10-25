3e8f10e2ec439731c3063195ccb1e96b428a3c80
rakuda-daraku <rakugaki.oukoku@gmail.com>
2017-10-25 13:45:19 +0900

    変数定義部分を `let` に変更

--- sample1/js/app.js
+++ sample1/js/app.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([43.067656, 141.350857], 10);
+let map = L.map('map').setView([43.067656, 141.350857], 10);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
